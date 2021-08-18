52f22d5c23c2651650db9670f400a22570a57ba0
koalakoker <koalakoker@gmail.com>
2021-08-18 08:58:58 +0200

    Watch single file from command line args

--- watch.js
+++ watch.js
@@ -2,31 +2,30 @@ import { getFileList, getGeneratedFileList } from "./scanDir.js";
 import fs from "fs";
 import { execFile } from 'child_process';
 
+process.argv.splice(0,2);
+const args = process.argv;
+if (args.length != 1) {
+  console.log("\nUsage node watch.js file\n");
+  process.exit(0);
+}
+
 // Watched file will be all .asy files present in current folder and subfolders
 const watchedFile = await getFileList();
-// Generated files will be all .asy file (name without extension) present in current folder except config.asy
-const onlyName = await getGeneratedFileList();
-// watchedFile.forEach(file => {
-//   onlyName.push(path.parse(file).name);
-// });
+const fileName = args[0];
 
 console.log(`Watching for file changes`);
 
+
 function fileChanged(curr, prev) {
-  console.log(`file Changed`);
+  console.log(`Source modified`);
 
-  onlyName.forEach(file => {
-    console.log(file);
-    execFile('./generate', [file], (err, stdout, stderr) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(file + ' generated correctly');
-      }
-    });
-    
+  execFile('./generate', [fileName], (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(fileName + ' generated correctly');
+    }
   });
-
 
 }
 
