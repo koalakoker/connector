6d9050a480e8d6a177bf5ee29e82dc462e5aad95
koalakoker <koalakoker@gmail.com>
2021-08-18 12:57:25 +0200

    First generation launched on start watching

--- watch.js
+++ watch.js
@@ -13,20 +13,26 @@ if (args.length != 1) {
 const watchedFile = await getFileList();
 const fileName = args[0];
 
+generation();
+
 console.log(`Watching for file changes`);
 
-
-function fileChanged(curr, prev) {
-  console.log(`Source modified`);
-
-  execFile('asy', ['-noV','-o','generated/',fileName], (err, stdout, stderr) => {
+function generation() {
+  console.log('Output generation');
+  
+  execFile('asy', ['-noV', '-o', 'generated/', fileName], (err, stdout, stderr) => {
     if (err) {
       console.log(err);
     } else {
       console.log(fileName + ' generated correctly');
     }
   });
+}
 
+
+function fileChanged(curr, prev) {
+  console.log(`Source modified`);
+  generation();
 }
 
 watchedFile.forEach(file => {
